b8be48248772c7c182fa11be6c97f4b2d672a631
wlMalk <wm.wexem@gmail.com>
2016-04-09 16:42:09 -0500

    Updated Mirango struct functions

--- mirango.go
+++ mirango.go
@@ -4,13 +4,12 @@ import (
 	"net/http"
 
 	"github.com/wlMalk/mirango/framework"
-	"github.com/wlMalk/mirango/internal/tree"
 	"github.com/wlMalk/mirango/server"
 )
 
 type Mirango struct {
 	server        framework.Server
-	node          *tree.Node
+	route         *Route
 	renderers     framework.Renderers
 	logger        framework.Logger
 	sessionStores []framework.SessionStore // add ability to make sessions on different stores
@@ -19,15 +18,17 @@ type Mirango struct {
 func New() *Mirango {
 	r := NewRoute("")
 	m := &Mirango{
-		node:       tree.New(r),
+		route:      r,
+		Operations: NewOperations(),
 	}
-	r.node = m.node
 	r.mirango = m
 	return m
 }
 
-func (m *Mirango) Renderer(r framework.Renderer) {
-	m.renderers.Add(r)
+func (m *Mirango) Renderers(r ...framework.Renderer) {
+	m.renderers.Append(r...)
+}
+
 }
 
 func (m *Mirango) Logger(l framework.Logger) {
@@ -40,3 +41,87 @@ func (m *Mirango) SessionStore(ss framework.SessionStore) {
 	}
 }
 
+func (m *Mirango) Params(params ...*Param) *Mirango {
+	m.route.params.Set(params...)
+	return m
+}
+
+func (m *Mirango) GetParams() *Params {
+	return m.route.params
+}
+
+func (m *Mirango) Route(path string) *Route {
+	r := NewRoute(path)
+	return m.AddRoute(r)
+}
+
+func (m *Mirango) AddRoute(r *Route) *Route {
+	r = m.route.AddRoute(r)
+	r.setMirango()
+	return r
+}
+
+func (m *Mirango) Path(path string) *Mirango {
+	m.route.SetPath(path)
+	return m
+}
+
+func (m *Mirango) Server(s framework.Server) {
+	m.server = s
+}
+
+func (m *Mirango) Start(addr string) error {
+	if m.server == nil {
+		m.server = server.New()
+	}
+	m.server.SetHandler(m)
+	m.server.SetLogger(m.logger)
+	m.server.SetAddr(addr)
+	return m.server.ListenAndServe()
+}
+
+func (m *Mirango) StartTLS(addr string, certFile string, keyFile string) error {
+	if m.server == nil {
+		m.server = server.New()
+	}
+	m.server.SetHandler(m)
+	m.server.SetLogger(m.logger)
+	m.server.SetAddr(addr)
+	return m.server.ListenAndServeTLS(certFile, keyFile)
+}
+
+func (m *Mirango) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	nr := NewRequest(r)
+	nw := NewResponse(w, m.renderers)
+	c := NewContext(nw, nr)
+
+	if m.logger != nil {
+		c.LogWriter = m.logger.Logger(c)
+	}
+
+	if m.sessionStores != nil {
+		var ses framework.Sessions
+		var err error
+
+		for _, ss := range m.sessionStores {
+			ses, err = ss.GetAll(r)
+			if err != nil {
+				// log
+			}
+			c.sessions.AddMany(ses...)
+		}
+	}
+
+	route, params := m.route.match(cleanPath(r.URL.Path)) // recommend redirection to standard path, tell if a match is found, otherwise return the latest found route
+	if route != nil {
+		data := route.ServeHTTP(c, params)
+		// check data type
+		if !c.ended {
+			err := nw.Render(c, data)
+			if err != nil {
+				return
+			}
+		}
+	} else {
+	}
+}
