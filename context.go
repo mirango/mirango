7c58d28585b13c851304c6a0d0fd0f065d854f20
wlMalk <wm.wexem@gmail.com>
2016-04-11 03:06:06 -0500

    Added values to Context struct

--- context.go
+++ context.go
@@ -11,6 +11,7 @@ type Context struct {
 	*Response
 	*Request
 	operation *Operation
+	values    framework.Values
 	ended     bool
 }
 
@@ -18,11 +19,28 @@ func NewContext(res *Response, req *Request) *Context {
 	return &Context{
 		Response: res,
 		Request:  req,
+		values:   framework.Values{},
 	}
 }
 
 func (c *Context) Operation() framework.Operation {
 	return c.operation
+}
+
+func (c *Context) Values() framework.Values {
+	return c.values
+}
+
+func (c *Context) SetValue(k interface{}, v interface{}) {
+	c.values.Set(k, v)
+}
+
+func (c *Context) GetValue(k interface{}) framework.Value {
+	return c.values.Get(k)
+}
+
+func (c *Context) Value(k interface{}) interface{} {
+	return c.values[k]
 }
 
 func (c *Context) Header() http.Header {
